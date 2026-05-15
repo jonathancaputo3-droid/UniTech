f4229bce57058881725d268ea94e549bfecf81ee
Jonny <jonathancaputo3@gmail.com>
2026-05-15 13:01:33 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/it/uid/unitech/Main.java
+++ src/main/java/it/uid/unitech/Main.java
@@ -1,9 +1,6 @@
 package it.uid.unitech;
 
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.application.Application;
-import org.controlsfx.glyphfont.FontAwesome;
 
 public class Main {
     public static void main(String[] args) {
