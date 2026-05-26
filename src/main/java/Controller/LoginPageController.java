a0080c0865ef66dbee81a07b403527ac75473cfb
Lorenzo <ioriolorenzo05@gmail.com>
2026-05-26 21:49:10 +0200

    Nuovo commit

--- src/main/java/Controller/LoginPageController.java
+++ src/main/java/Controller/LoginPageController.java
@@ -4,20 +4,15 @@ import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.util.Duration;
-import org.kordamp.ikonli.javafx.FontIcon;
-
 
 
 public class LoginPageController {
 
     @FXML
     private Button loginButton;
+    @FXML
+    private Label signUp;
 
     public void initialize() {
 
@@ -35,5 +30,17 @@ public class LoginPageController {
             scaleUp.play();
         });
 
+        signUp.setOnMouseEntered(event -> {
+            ScaleTransition scaleDown= new ScaleTransition(Duration.millis(150),signUp);
+            scaleDown.setToX(0.9);
+            scaleDown.setToY(0.9);
+            scaleDown.play();
+        });
+        signUp.setOnMouseExited(event -> {
+            ScaleTransition scaleUp = new ScaleTransition(Duration.millis(150),signUp);
+            scaleUp.setToX(1);
+            scaleUp.setToY(1);
+            scaleUp.play();
+        });
     }
 }
