2f16c85290da9b801270fdb7ed3eae290a285b97
Jonny <jonathancaputo3@gmail.com>
2026-07-10 17:38:08 +0200

    Descrizione delle modifiche

--- src/main/java/Controller/WatchProductController.java
+++ src/main/java/Controller/WatchProductController.java
@@ -130,6 +130,48 @@ public class WatchProductController {
     private ImageView logoIconUltra;
     @FXML
     private ImageView immagineProdottoUltra;
+    @FXML
+    private Label prezzoLabelSE;
+    @FXML
+    private Label coloreLabelSE;
+    @FXML
+    private Circle circleNeroSE;
+    @FXML
+    private Circle circleBluSE;
+    @FXML
+    private Circle circleGialloSE;
+    @FXML
+    private Circle circleRosaSE;
+    @FXML
+    private Button btnGPSSE;
+    @FXML
+    private Button btnGPSCellularSE;
+    @FXML
+    private FontIcon loginIconSE;
+    @FXML
+    private ImageView logoIconSE;
+    @FXML
+    private ImageView immagineProdottoSE;
+    @FXML
+    private Label prezzoLabel11;
+    @FXML
+    private Label coloreLabel11;
+    @FXML
+    private Circle circleNero11;
+    @FXML
+    private Circle circleGrigio11;
+    @FXML
+    private Circle circleOro11;
+    @FXML
+    private Button btnGPS11;
+    @FXML
+    private Button btnGPSCellular11;
+    @FXML
+    private FontIcon loginIcon11;
+    @FXML
+    private ImageView logoIcon11;
+    @FXML
+    private ImageView immagineProdotto11;
 
 
     public void initialize(){
@@ -137,20 +179,54 @@ public class WatchProductController {
         AnimazioneUtil.aggiungiClickColore(circleNeroUltra, "Nero", "/it/uid/unitech/Images/Apple Watch Ultra 3 Black.png", coloreLabelUltra, immagineProdottoUltra);
         AnimazioneUtil.aggiungiClickBottone(btnGPSUltra, "759€", prezzoLabelUltra);
 
+        AnimazioneUtil.aggiungiClickColore(circleGrigio11, "Naturale", "/it/uid/unitech/Images/Apple Watch Series 11 Naturale.png", coloreLabel11, immagineProdotto11);
+        AnimazioneUtil.aggiungiClickBottone(btnGPS11, "759€", prezzoLabel11);
+
+        AnimazioneUtil.aggiungiClickColore(circleNeroSE, "Nero", "/it/uid/unitech/Images/Apple Watch SE 3 Black.png", coloreLabelSE, immagineProdottoSE);
+        AnimazioneUtil.aggiungiClickBottone(btnGPSSE, "279€", prezzoLabelSE);
+
         AnimazioneUtil.aggiungiClickColore(circleVerdeUltra, "Verde Neon", "/it/uid/unitech/Images/Apple Watch Ultra 3 Green.png", coloreLabelUltra, immagineProdottoUltra);
 
         AnimazioneUtil.aggiungiClickColore(circleBluUltra, "Blu Navy", "/it/uid/unitech/Images/Apple Watch Ultra 3 Blu.png", coloreLabelUltra, immagineProdottoUltra);
 
+        AnimazioneUtil.aggiungiClickColore(circleRosaSE, "Rosa Fard", "/it/uid/unitech/Images/Apple Watch SE 3 Pink.png", coloreLabelSE, immagineProdottoSE);
+
+        AnimazioneUtil.aggiungiClickColore(circleGialloSE, "Giallo Neon", "/it/uid/unitech/Images/Apple Watch SE 3 Yellow.png", coloreLabelSE, immagineProdottoSE);
+
+        AnimazioneUtil.aggiungiClickColore(circleBluSE, "Blu Salmastro", "/it/uid/unitech/Images/Apple Watch SE 3 Blu.png", coloreLabelSE, immagineProdottoSE);
+
+        AnimazioneUtil.aggiungiClickColore(circleOro11, "Oro", "/it/uid/unitech/Images/Apple Watch Series 11 Oro.png", coloreLabel11, immagineProdotto11);
+
+        AnimazioneUtil.aggiungiClickColore(circleNero11, "Ardesia", "/it/uid/unitech/Images/Apple Watch Series 11 Ardesia.png", coloreLabel11, immagineProdotto11);
+
+        AnimazioneUtil.aggiungiClickBottone(btnGPSCellular11, "859€", prezzoLabel11);
+
         AnimazioneUtil.aggiungiClickBottone(btnGPSCellularUltra,"859€",prezzoLabelUltra);
 
+        AnimazioneUtil.aggiungiClickBottone(btnGPSCellularSE, "379€", prezzoLabelSE);
 
         AnimazioneUtil.aggiungiAnimazione(loginIconUltra);
 
+        AnimazioneUtil.aggiungiAnimazione(loginIconSE);
+
+        AnimazioneUtil.aggiungiAnimazione(loginIcon11);
+
         AnimazioneUtil.verificaCambiaScena(loginIconUltra, "/Fxml/ProfileOption.fxml");
+
+        AnimazioneUtil.verificaCambiaScena(loginIconSE, "/Fxml/ProfileOption.fxml");
+
+        AnimazioneUtil.verificaCambiaScena(loginIcon11, "/Fxml/ProfileOption.fxml");
+
+        AnimazioneUtil.aggiungiAnimazione(logoIconSE);
 
         AnimazioneUtil.aggiungiAnimazione(logoIconUltra);
 
+        AnimazioneUtil.aggiungiAnimazione(logoIcon11);
+
+        AnimazioneUtil.verificaCambiaScena(logoIconSE, "/Fxml/Home.fxml");
+
         AnimazioneUtil.verificaCambiaScena(logoIconUltra, "/Fxml/Home.fxml");
 
+        AnimazioneUtil.verificaCambiaScena(logoIcon11, "/Fxml/Home.fxml");
     }
 }
