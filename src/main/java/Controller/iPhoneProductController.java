ef29cca960d8967fdd1e07ec8c78340b1a791bb1
gdefil05 <dflgrl05e24f537b@studenti.unical.it>
2026-07-13 19:33:09 +0200

    iPhone Single Product

--- src/main/java/Controller/iPhoneProductController.java
+++ src/main/java/Controller/iPhoneProductController.java
@@ -108,71 +108,113 @@ public class iPhoneProductController {
 
         fadeOut.play();
     }
+    //DATI iPnone 17
+    @FXML
+    private ImageView immagineProdotto17;
+    @FXML
+    private Label prezzoLabel17;
+    @FXML
+    private Label coloreLabel17;
+    @FXML
+    private Circle circleNero17;
+    @FXML
+    private Circle circleBlu17;
+    @FXML
+    private Circle circleBianco17;
+    @FXML
+    private Circle circleVerde17;   
+    @FXML
+    private Circle circleRosa17;
+    @FXML
+    private Button btn128_17;
+    @FXML
+    private Button btn256_17;
+    @FXML
+    private Button btn512_17;
+    @FXML
+    private FontIcon loginIcon17;
+    @FXML
+    private ImageView logoIcon17;
+
+    //DATI iPnone 17 Pro
 
     @FXML
-    private ImageView immagineProdotto;
+    private ImageView immagineProdotto17Pro;
     @FXML
-    private Label prezzoLabel;
+    private Label prezzoLabel17Pro;
     @FXML
-    private Label coloreLabel;
+    private Label coloreLabel17Pro;    
     @FXML
-    private Circle circleNero;
+    private Circle circleArancione17Pro;
     @FXML
-    private Circle circleBlu;
+    private Circle circleBianco17Pro;
     @FXML
-    private Circle circleBianco;
+    private Circle circleBlu17Pro;
     @FXML
-    private Circle circleVerde;
+    private Button btn256_17Pro;
     @FXML
-    private Button btn128;
+    private Button btn512_17Pro;
     @FXML
-    private Button btn256;
+    private Button btn1TB_17Pro;
     @FXML
-    private Button btn512;
+    private FontIcon loginIcon17Pro;
     @FXML
-    private FontIcon loginIcon;
-    @FXML
-    private ImageView logoIcon;
+    private ImageView logoIcon17Pro;   
 
-    public void initialize(){
+    //DATI iPnone 17 Air
+    @FXML
+    private ImageView immagineProdottoAir;
+    @FXML
+    private Label prezzoLabelAir;
+    @FXML
+    private Label coloreLabelAir;    
+    @FXML
+    private Circle circleNeroAir;
+    @FXML
+    private Circle circleBiancoAir;
+    @FXML
+    private Circle circleCelesteAir;
+    @FXML
+    private Circle circleOroAir;
+    @FXML
+    private Button btn256Air;
+    @FXML
+    private Button btn512Air;
+    @FXML
+    private Button btn1TBAir;
+    @FXML
+    private FontIcon loginIconAir;
+    @FXML
+    private ImageView logoIconAir;  
 
-        AnimazioneUtil.selezionaColore(circleNero,"Nero","/it/uid/unitech/Images/IPhone 17 Black.png",coloreLabel,immagineProdotto);
-        AnimazioneUtil.selezionaVariante(btn128,"999€",prezzoLabel);
-        coloreLabel.setText("Nero");
+    public void initialize()
+    {
+    //Prodotto iPhone 17
+        AnimazioneUtil.aggiungiClickColore(circleNero17, "Nero", "/it/uid/unitech/Images/iPhone 17 Nero.png", coloreLabel17, immagineProdotto17);
+        AnimazioneUtil.aggiungiClickColore(circleBlu17, "Blu", "/it/uid/unitech/Images/iPhone 17 Azzurro.png", coloreLabel17, immagineProdotto17);
+        AnimazioneUtil.aggiungiClickColore(circleBianco17, "Bianco", "/it/uid/unitech/Images/iPhone 17 Bianco.png", coloreLabel17, immagineProdotto17);
+        AnimazioneUtil.aggiungiClickColore(circleVerde17, "Verde", "/it/uid/unitech/Images/iPhone 17 Verde.png", coloreLabel17, immagineProdotto17);
+        AnimazioneUtil.aggiungiClickColore(circleRosa17, "Rosa", "/it/uid/unitech/Images/iPhone 17 Rosa.png", coloreLabel17, immagineProdotto17);
+        AnimazioneUtil.aggiungiClickBottone(btn128_17, "939€", prezzoLabel17);
+        AnimazioneUtil.aggiungiClickBottone(btn256_17, "1.039€", prezzoLabel17);
+        AnimazioneUtil.aggiungiClickBottone(btn512_17, "1.239€", prezzoLabel17);
 
-        circleNero.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaColore(circleNero,"Nero","/it/uid/unitech/Images/IPhone 17 Black.png",coloreLabel,immagineProdotto);
-        });
-        circleBianco.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaColore(circleBianco,"Bianco","/it/uid/unitech/Images/IPhone 17 Bianco.png",coloreLabel,immagineProdotto);
-        });
-        circleVerde.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaColore(circleVerde,"Salvia","/it/uid/unitech/Images/iPhone 17 Verde.png",coloreLabel,immagineProdotto);
-        });
-        circleBlu.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaColore(circleBlu,"Azzurro","/it/uid/unitech/Images/iPhone 17 Blu.png",coloreLabel,immagineProdotto);
-        });
+    //Prodotto iPhone 17 Pro
+        AnimazioneUtil.aggiungiClickColore(circleArancione17Pro, "Arancione", "/it/uid/unitech/Images/iPhone 17 Pro Arancione.png", coloreLabel17Pro, immagineProdotto17Pro);
+        AnimazioneUtil.aggiungiClickColore(circleBianco17Pro, "Bianco", "/it/uid/unitech/Images/iPhone 17 Pro Bianco.png", coloreLabel17Pro, immagineProdotto17Pro);
+        AnimazioneUtil.aggiungiClickColore(circleBlu17Pro, "Blu", "/it/uid/unitech/Images/iPhone 17 Pro Blu.png", coloreLabel17Pro, immagineProdotto17Pro);
+        AnimazioneUtil.aggiungiClickBottone(btn256_17Pro, "1.339€", prezzoLabel17Pro);
+        AnimazioneUtil.aggiungiClickBottone(btn512_17Pro, "1.539€", prezzoLabel17Pro);
+        AnimazioneUtil.aggiungiClickBottone(btn1TB_17Pro, "1.839€", prezzoLabel17Pro);
 
-        btn128.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaVariante(btn128,"999€",prezzoLabel);
-        });
-        btn256.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaVariante(btn256,"1099€",prezzoLabel);
-        });
-        btn512.setOnMouseClicked(event -> {
-            AnimazioneUtil.selezionaVariante(btn512,"1299€",prezzoLabel);
-        });
+    //Prodotto iPhone Air
+        AnimazioneUtil.aggiungiClickColore(circleNeroAir, "Nero", "/it/uid/unitech/Images/iPhone Air Nero.png", coloreLabelAir, immagineProdottoAir);
+        AnimazioneUtil.aggiungiClickColore(circleBiancoAir, "Bianco", "/it/uid/unitech/Images/iPhone Air Bianco.png", coloreLabelAir, immagineProdottoAir);
+        AnimazioneUtil.aggiungiClickColore(circleCelesteAir, "Celeste", "/it/uid/unitech/Images/iPhone Air Celeste.png", coloreLabelAir, immagineProdottoAir);
+        AnimazioneUtil.aggiungiClickColore(circleOroAir, "Oro", "/it/uid/unitech/Images/iPhone Air Oro.png", coloreLabelAir, immagineProdottoAir);
+        AnimazioneUtil.aggiungiClickBottone(btn256Air, "1.219€", prezzoLabelAir);
+        AnimazioneUtil.aggiungiClickBottone(btn512Air, "1.419€", prezzoLabelAir);  
+        AnimazioneUtil.aggiungiClickBottone(btn1TBAir, "1.619€", prezzoLabelAir);
 
-        AnimazioneUtil.aggiungiAnimazioneScale(loginIcon);
-
-        loginIcon.setOnMouseClicked(event -> {
-            AnimazioneUtil.cambiaScena(loginIcon, "/Fxml/ProfileOption.fxml");
-        });
-
-        AnimazioneUtil.aggiungiAnimazioneScale(logoIcon);
-
-        logoIcon.setOnMouseClicked(event -> {
-            AnimazioneUtil.cambiaScena(logoIcon, "/Fxml/Home.fxml");
-        });
     }
 }
--- src/main/java/it/uid/unitech/UniTech.java
+++ src/main/java/it/uid/unitech/UniTech.java
@@ -15,7 +15,7 @@ public class UniTech extends Application {
     public void start(Stage stage) throws Exception {
 
         FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/Fxml/Apple Watch Series 11 PageProduct.fxml")
+                getClass().getResource("/Fxml/iPhone Air PageProduct.fxml")
         );
 
         Parent root = loader.load();
